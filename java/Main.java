48c5bb22990d982757ee04b3a2b8bcecb20adf68
yusuke.kato@persol.co.jp <yusuke.kato@persol.co.jp>
2020-07-14 09:54:17 +0000

    test

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,6 @@
 class Main {
  public static void main(String[] args) {
+   System.out.println("start.");
    for (int i=0; i <10 ; i++) {
      System.out.println("Hello, Beatutiful world.");
    }
